ff1d16be3286949b5056f4441a9e5cbc0b8ed3d7
Tagirov2004 <98663516+Tagirov2004@users.noreply.github.com>
2024-06-28 13:46:58 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        Console.Write("Введите количество бенгальских огней у Игоря (c1): ");
+        int c1 = int.Parse(Console.ReadLine());
+        Console.Write("Введите количество потухших огоньков (b1): ");
+        int b1 = int.Parse(Console.ReadLine());
+
+        int totalBurningTime = OptimalBurningTime(c1, b1);
+
+        Console.WriteLine($"Общее время горения огней: {totalBurningTime} часов");
+    }
+
+    static int OptimalBurningTime(int c1, int b1)
+    {
+        // Сжигаем первый набор из c1 огоньков
+        int totalTime = c1 * 2;
+
+        // Создаем новые огни из потухших огоньков
+        int newFireworks = b1 / 2;
+        totalTime += newFireworks * 2;
+
+        return totalTime;
+    }
+}
